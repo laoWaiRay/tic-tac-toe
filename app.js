f149c2d596074a059ee22b529f04801eb2a98777
Ray <raymondly96@gmail.com>
2022-06-26 14:34:37 -0700

    Fix Computer Algorithm which caused it to be beatable

--- app.js
+++ app.js
@@ -136,6 +136,126 @@ const gameController = function(player1, player2){
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////// Computer Player ///////////////////////////////////////////////////
+
+const computer = (function () {
+    const rows = [[0,1,2],[3,4,5],[6,7,8]];
+    const cols = [[0,3,6],[1,4,7],[2,5,8]];
+    const diagonals = [[0,4,8], [2,4,6]];
+    let lastPlayedComputerPosition = 0;
+
+    const makeRandMove = function(){
+        const currentSpaces = gameBoard.getGameboard();
+        const openSpaces = [];
+        for (i=0; i<currentSpaces.length; i++) {
+            if (currentSpaces[i] === 0) openSpaces.push(i);
+        }
+        
+        const getRandomSpace = () => {
+            const randSpaceIndex = Math.floor(Math.random()*openSpaces.length);
+            return openSpaces[randSpaceIndex];
+        }
+
+        const randSpace = getRandomSpace();
+        gameBoard.updateGameboard(randSpace);
+        lastPlayedComputerPosition = randSpace;
+    }
+
+    const makeMove = function(lastPlayedPosition){
+        const checkedPlayerLines = [];
+        const checkedComputerLines = [];
+
+        if(rows[0].includes(lastPlayedPosition)) checkedPlayerLines.push(rows[0])
+        if(rows[1].includes(lastPlayedPosition)) checkedPlayerLines.push(rows[1])
+        if(rows[2].includes(lastPlayedPosition)) checkedPlayerLines.push(rows[2])
+
+        if(cols[0].includes(lastPlayedPosition)) checkedPlayerLines.push(cols[0])
+        if(cols[1].includes(lastPlayedPosition)) checkedPlayerLines.push(cols[1])
+        if(cols[2].includes(lastPlayedPosition)) checkedPlayerLines.push(cols[2])
+
+        if(diagonals[0].includes(lastPlayedPosition)) checkedPlayerLines.push(diagonals[0])
+        if(diagonals[1].includes(lastPlayedPosition)) checkedPlayerLines.push(diagonals[1])
+
+
+
+        if(rows[0].includes(lastPlayedComputerPosition)) checkedComputerLines.push(rows[0])
+        if(rows[1].includes(lastPlayedComputerPosition)) checkedComputerLines.push(rows[1])
+        if(rows[2].includes(lastPlayedComputerPosition)) checkedComputerLines.push(rows[2])
+
+        if(cols[0].includes(lastPlayedComputerPosition)) checkedComputerLines.push(cols[0])
+        if(cols[1].includes(lastPlayedComputerPosition)) checkedComputerLines.push(cols[1])
+        if(cols[2].includes(lastPlayedComputerPosition)) checkedComputerLines.push(cols[2])
+
+        if(diagonals[0].includes(lastPlayedComputerPosition)) checkedComputerLines.push(diagonals[0])
+        if(diagonals[1].includes(lastPlayedComputerPosition)) checkedComputerLines.push(diagonals[1])
+
+
+
+        if(gameBoard.getGameboard()[4] === 0){
+            gameBoard.getGameboard()[4] = -1;
+            gameBoard.updateGameboard();
+            lastPlayedComputerPosition = 4;
+            return;
+        }
+
+        for (const line of checkedComputerLines){
+            let total = 0;
+            for(const index of line){
+                total += gameBoard.getGameboard()[index];
+            }
+            
+            if(total === -2) {
+                for(const index of line){
+                    if(gameBoard.getGameboard()[index] === 0) {
+                        gameBoard.getGameboard()[index] = parseInt(-1);
+                        gameBoard.updateGameboard();
+                        lastPlayedComputerPosition = index;
+                        return;
+                    }
+                }
+            }
+        }
+
+        for (const line of checkedPlayerLines){
+            let total = 0;
+            for(const index of line){
+                total += gameBoard.getGameboard()[index];
+            }
+            
+            if(total === 2) {
+                for(const index of line){
+                    if(gameBoard.getGameboard()[index] === 0) {
+                        gameBoard.getGameboard()[index] = parseInt(-1);
+                        gameBoard.updateGameboard();
+                        lastPlayedComputerPosition = index;
+                        return;
+                    }
+                }
+            }
+        }
+
+        makeRandMove();
+    }
+
+    const getLastPlayedComputerPosition = () => lastPlayedComputerPosition;
+
+    return{
+        makeMove,
+        getLastPlayedComputerPosition
+    }
+
+})()
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 // Event Handlers //
 
@@ -145,7 +265,25 @@ gridSpaces.forEach(gridSpace => {
             if(e.target.innerText != '') return
             const gridSpaceIndexNumber = e.target.dataset.index;            // This is a string
             gameBoard.updateGameboard(gridSpaceIndexNumber);
-            const winner = gameController.checkWinner(parseInt(gridSpaceIndexNumber));     // parseInt the string into an integer
+            let winner = gameController.checkWinner(parseInt(gridSpaceIndexNumber));     // parseInt the string into an integer
+            if (winner) {
+                const div = document.createElement('div');
+                div.classList.add('msg-win')
+                div.classList.add('delete-me')
+                div.append(`${winner} Wins!`);
+                body.append(div);
+                return;
+            } else if (isGameOver === true) {
+                const div = document.createElement('div');
+                div.classList.add('msg-tie')
+                div.classList.add('delete-me')
+                div.append('It\'s a tie!');
+                body.append(div);
+            }
+
+            computer.makeMove(parseInt(gridSpaceIndexNumber));              // computer makes move
+            
+            winner = gameController.checkWinner(computer.getLastPlayedComputerPosition());
             if (winner) {
                 const div = document.createElement('div');
                 div.classList.add('msg-win')
@@ -166,7 +304,10 @@ gridSpaces.forEach(gridSpace => {
 
 resetBtn.onclick = (e) => {
     gameBoard.createNewGameboard();
-    document.querySelector('.delete-me').remove();
+    if(document.contains(document.querySelector('.delete-me'))){
+        document.querySelector('.delete-me').remove();
+    }
+    
 }
 
 
